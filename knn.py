3a6f3ae0496edc0afd9f5af1b21bd5dba354390f
Hetens <92987462+Hetens@users.noreply.github.com>
2023-06-15 11:48:45 +0530

    ayo

--- knn.py
+++ knn.py
@@ -1,30 +1,56 @@
+#euclidian distance
+from math import sqrt
 import numpy as np
-from sklearn import neighbors, preprocessing
-from sklearn.model_selection import train_test_split
+import warnings
+from collections import Counter
 import pandas as pd
-import matplotlib.pyplot as plt
+import random
+# for i in dataset:
+#     for ii in dataset[i]:
+#         plt.scatter(ii[0],ii[1], s=100, color=i)
+# plt.scatter(new_features[0],new_features[1])
+# plt.show()
 
-df = pd.read_csv('breast-cancer-wisconsin.data') # read data
-df.replace('?', -99999, inplace=True) # replace missing data with outlier')
-df.drop(['id'], 1, inplace=True) # drop id column'
-X = np.array(df.drop(['class'], 1)) # features
-y = np.array(df['class']) # labels
+def k_nearest_neighbors(data, predict, k=3):
+    if len(data) >= k:
+        warnings.warn('K is set to a value less than total voting groups')
+    distances = []
+    for group in data:
+        for features in data[group]:
+            #euclidian_distance = sqrt( (features[0]-predict[0])**2 + (features[1]-predict[1])**2 )
+            #euclidian_distance = np.sqrt(np.sum((np.array(features)-np.array(predict))**2))
+            euclidian_distance = np.linalg.norm(np.array(features)-np.array(predict))
+            distances.append([euclidian_distance, group])
+    votes = [i[1] for i in sorted(distances)[:k]]
+    #print(Counter(votes).most_common(1))
+    vote_result = Counter(votes).most_common(1)[0][0]
+    return vote_result
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2) # split data into training and testing samples
+df = pd.read_csv('breast-cancer-wisconsin.data')
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
 
-clf =neighbors.KNeighborsClassifier() # classifier
-clf.fit(X_train, y_train) # train classifier
+full_data = df.astype(float).values.tolist()
+random.shuffle(full_data)
 
-accuracy = clf.score(X_test, y_test) # test classifier
-print(accuracy)
+test_size = 0.2
+train_set ={2:[], 4:[]}
+test_set = {2:[], 4:[]}
+train_data = full_data[:-int(test_size*len(full_data))]#first 80%
+test_data = full_data[-int(test_size*len(full_data)):]#last 20%
 
-example_measures = np.array([[4,2,1,1,1,2,3,2,1], [4,2,1,2,2,2,3,2,1]]) # example data
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
 
-prediction = clf.predict(example_measures) # predict class of example data
-print(prediction)
-# Plotting the dataset
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap='coolwarm')
-plt.xlabel('Feature 0')
-plt.ylabel('Feature 1')
-plt.title('Breast Cancer Wisconsin Dataset')
-plt.show()
+correct =0
+total =0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote = k_nearest_neighbors(train_set, data, k=5)
+        if group == vote:
+            correct+=1
+        total+=1
+print('Accuracy:', correct/total)
