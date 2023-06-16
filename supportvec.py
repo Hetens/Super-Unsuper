43be5199936e353bb8aa653a5f0de97a96556ed7
Hetens <92987462+Hetens@users.noreply.github.com>
2023-06-16 12:09:01 +0530

    shiny graphs

--- supportvec.py
+++ supportvec.py
@@ -69,5 +69,40 @@ class Support_Vector_Machine:
     def predict(self, features):
         #sign(x.w+b)
         classification = np.sign(np.dot(np.array(features),self.w)+self.b)
+        if classification !=0 and self.visualization:
+            self.ax.scatter(features[0],features[1],s=200,marker='*',c = self.colors[classification])
         return classification
+    def visualize(self):
+        [[self.ax.scatter(x[0],x[1], s=100, color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+        #hyperplane = x.w+b
+        def hyperplane(x,w,b,v):
+            return (-w[0]*x-b+v)/w[1]
+        datarange = (self.min_feature_value*0.9,self.max_feature_value*1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+
+        #positive support vector hyperplane (w.x+b) = 1
+        psv1 = hyperplane(hyp_x_min,self.w,self.b,1)
+        psv2 = hyperplane(hyp_x_max,self.w,self.b,1)
+        self.ax.plot([hyp_x_min,hyp_x_max],[psv1,psv2],'k')
+
+        #negative support vector hyperplane (w.x+b) = -1
+        nsv1 = hyperplane(hyp_x_min,self.w,self.b,-1)
+        nsv2 = hyperplane(hyp_x_max,self.w,self.b,-1)
+        self.ax.plot([hyp_x_min,hyp_x_max],[nsv1,nsv2],'k')
+
+        #decision boundary (w.x+b) = 0
+        db1 = hyperplane(hyp_x_min,self.w,self.b,0)
+        db2 = hyperplane(hyp_x_max,self.w,self.b,0)
+        self.ax.plot([hyp_x_min,hyp_x_max],[db1,db2],'y--')
+
+        plt.show()
+    
 data_dict = {-1:np.array([[1,7],[2,8],[3,8]]), 1:np.array([[5,1],[6,-1],[7,3]])}
+
+svm = Support_Vector_Machine()
+svm.fit(data=data_dict)
+predict_us = [[0,10],[1,3],[3,4],[3,5],[5,5],[5,6],[6,-5],[5,8]]
+for p in predict_us:
+    svm.predict(p)
+svm.visualize()
