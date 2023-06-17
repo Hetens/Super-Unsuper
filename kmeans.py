0e7086e4dd33b576ab9a06dd3d7dde41432346b7
Hetens <92987462+Hetens@users.noreply.github.com>
2023-06-17 15:10:43 +0530

    k means implementaiton

--- kmeans.py
+++ kmeans.py
@@ -12,6 +12,46 @@ X = np.array([[1,2],
                 [9,11]])
 #plt.scatter(X[:,0],X[:,1],s=150)
 #plt.show()
+class K_Means:
+    def __init__(self, tol = 0.01, k=2,max_iter=300):
+        self.k = k
+        self.tol = tol
+        self.max_iter = max_iter
+    def fit(self,data):
+        self.centroids = {}
+        for i in range(self.k):
+            self.centroids[i] = data[i]
+        for i in range(self.max_iter):
+            self.classifications ={}
+            for i in range(self.k):
+                self.classifications[i] = []
+            for featureset in data:
+                distances = [np.linalg.norm(featureset-self.centroids[centroid]) for centroid in self.centroids]
+                classification = distances.index(min(distances))
+                self.classifications[classification].append(featureset)
+            prev_centroids = dict(self.centroids)
+            for classification in self.classifications:
+                self.centroids[classification] = np.average(self.classifications[classification],axis=0)
+            optimized = True
+            for c in self.centroids:
+                original_centroid = prev_centroids[c]
+                current_centroid = self.centroids[c]
+                if np.sum((current_centroid-original_centroid)/original_centroid*100.0) > self.tol:
+                    optimized = False
+            if optimized:
+                break
+    def predict(self,data):
+        distances = [np.linalg.norm(data-self.centroids[centroid]) for centroid in self.centroids]
+        classification = distances.index(min(distances))
+        return classification
+colors = ["g","r","c","y"]
+clf = K_Means()
+clf.fit(X)
 
-colors = ["g.","r.","c.","y."]
+for centroid in clf.centroids:
+    plt.scatter(clf.centroids[centroid][0],clf.centroids[centroid][1],marker="o",color="k",s=150,linewidths=5)
+for classification in clf.classifications:
+    color = colors[classification]
+    for featureset in clf.classifications[classification]:
+        plt.scatter(featureset[0],featureset[1],marker="x",color=color,s=150,linewidths=5)
 plt.show()
