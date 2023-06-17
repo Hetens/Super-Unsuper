176312ef407ccf18445f06f7d6d6b9ca446b1e21
Hetens <92987462+Hetens@users.noreply.github.com>
2023-06-17 13:23:04 +0530

    same repo??

--- kmeans.py
+++ kmeans.py
@@ -13,12 +13,5 @@ X = np.array([[1,2],
 #plt.scatter(X[:,0],X[:,1],s=150)
 #plt.show()
 
-clf = KMeans(n_clusters=2)
-clf.fit(X)
-centroids = clf.cluster_centers_
-labels = clf.labels_
 colors = ["g.","r.","c.","y."]
-for i in range(len(X)):
-    plt.plot(X[i][0],X[i][1],colors[labels[i]],markersize=10)
-plt.scatter(centroids[:,0],centroids[:,1],marker='x',s=150,linewidths=5)
 plt.show()
