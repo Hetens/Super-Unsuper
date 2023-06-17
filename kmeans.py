d18e7fa6ad98271fa9fae690ffe8ebf3ac4e2cac
Hetens <92987462+Hetens@users.noreply.github.com>
2023-06-17 15:18:59 +0530

    finished kmeans

--- kmeans.py
+++ kmeans.py
@@ -54,4 +54,12 @@ for classification in clf.classifications:
     color = colors[classification]
     for featureset in clf.classifications[classification]:
         plt.scatter(featureset[0],featureset[1],marker="x",color=color,s=150,linewidths=5)
+unknowns = np.array([[1,3],
+                    [8,9],
+                    [0,3],
+                    [5,4],
+                    [6,4],])
+for unknown in unknowns:
+    classification = clf.predict(unknown)
+    plt.scatter(unknown[0],unknown[1],marker="*",color=colors[classification],s=150,linewidths=5)
 plt.show()
