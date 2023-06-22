e5ee13e16656006a9082bf6dd8a2491ed45096dc
Hetens <92987462+Hetens@users.noreply.github.com>
2023-06-22 12:50:49 +0530

    SoftMax

--- neural_net.py
+++ neural_net.py
@@ -23,9 +23,19 @@ class Layer_Dense:
 class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
 
-layer1 = Layer_Dense(2,5)
+dense1 = Layer_Dense(2,3)
 activation1 = Activation_ReLU()
-layer1.forward(X)
-activation1.forward(layer1.output)
-print(activation1.output)
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+print(activation2.output[:5])
